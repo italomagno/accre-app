ed93e8bb210048640ba8e96ab98ac7d0fe6c4def
Italo Magno <italomagno10@gmail.com>
2023-11-23 07:15:53 -0300

    changes on popover content

--- src/components/shifts/ShftPopOver.tsx
+++ src/components/shifts/ShftPopOver.tsx
@@ -8,7 +8,8 @@ import {
   ModalCloseButton,
   Button,
   Flex,
-  Text,
+  Heading,
+  Box,
   HStack,
   VStack,
   useDisclosure
@@ -44,15 +45,50 @@ export function ShiftPopOver({ children, handleSelectedShift }: ShiftPopOverProp
 
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay bg='blackAlpha.600' />
-        <ModalContent>
-          <ModalHeader>Escolha um Turno ou Afastamento</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
+        <ModalContent
+        w={"50vw"}
+        my={"auto"}
+        mx={"auto"}
+        bg={"white"}
+
+        >
+          <ModalHeader
+          
+          py={3}
+          px={4}
+          mb={3}
+          >
+            <Flex
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            >
+              <Box><Heading
+              fontWeight={"extrabold"}
+              >
+              Escolha um: 
+              </Heading></Box>
+              <Box> <ModalCloseButton /></Box>
+            </Flex>
+            
+            </ModalHeader>
+          <ModalBody
+          py={3}
+          px={4}
+          >
             <Flex flexDir={"column"}>
-              <Button colorScheme={selectedShiftType === "shift" ? "blue" : "gray"}
+
+
+              <Flex
+              w={"full"}
+              justifyContent={"space-around"}
+              mb={3}
+              >
+                <Button w="35%"colorScheme={selectedShiftType === "shift" ? "blue" : "gray"}
                 onClick={() => handleShiftTypeSelection("shift")}>Turno</Button>
-              <Button colorScheme={selectedShiftType === "absence" ? "blue" : "gray"}
+              <Button w="35%" colorScheme={selectedShiftType === "absence" ? "blue" : "gray"}
                 onClick={() => handleShiftTypeSelection("absence")}>Afastamento</Button>
+
+              </Flex>
 
               {selectedShiftType === "shift" && (
                 <VStack spacing={2} mt={3}>
@@ -71,14 +107,19 @@ export function ShiftPopOver({ children, handleSelectedShift }: ShiftPopOverProp
               {/* Implemente aqui a lógica para escolha de Afastamento */}
             </Flex>
           </ModalBody>
-          <ModalFooter>
+          <ModalFooter
+          py={3}
+          px={4}
+          >
           <Button colorScheme="red" onClick={()=>{
             handleShiftSelection(" - ")
             onClose()
             }}>
               Excluir
             </Button>
-            <Button colorScheme="blue" onClick={onClose}>
+            <Button
+            ml={3}
+            colorScheme="blue" onClick={onClose}>
               Fechar
             </Button>
           </ModalFooter>
