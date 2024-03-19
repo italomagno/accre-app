62139eef1a2762b31a7c93011b718e509e9ad7ae
Italo Magno <italomagno10@gmail.com>
2024-03-19 09:01:42 -0300

    Vercel Analytics Installed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider} from '@chakra-ui/provider'
 import { theme } from '@/styles/globals'
 import { SessionProvider } from "next-auth/react"
 import { createStandaloneToast } from '@chakra-ui/toast';
+import { Analytics } from "@vercel/analytics/react"
 
 
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   const { ToastContainer } = createStandaloneToast();
   return (
     <SessionProvider session={session}>
+      <Analytics/>
       <ToastContainer />
       <ChakraProvider theme={theme} >
         <Component {...pageProps} />
