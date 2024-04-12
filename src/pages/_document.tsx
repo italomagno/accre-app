3b8d54f109ae4e6888ca3d095bef863dff82fb45
Italo Magno <italomagno10@gmail.com>
2024-04-11 21:25:12 -0300

    adding google add sense in app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='pt'  >
-      <Head />
+      <Head >
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9446204860071784"
+     crossOrigin="anonymous"></script>
+     <meta name="google-adsense-account" content="ca-pub-9446204860071784"></meta>
+      </Head>
       <body >
         {/* 👇 Here's the script */}
         <Main />
